e47d99f4209d6a42751884a83a24155c4ea7095d
Tobin <tobinjbrown@gmail.com>
2014-04-23 14:47:44 -0500

    First update :q

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -39,14 +39,6 @@ KeyboardInputManager.prototype.listen = function () {
     39: 1, // Right
     40: 2, // Down
     37: 3, // Left
-    75: 0, // Vim up
-    76: 1, // Vim right
-    74: 2, // Vim down
-    72: 3, // Vim left
-    87: 0, // W
-    68: 1, // D
-    83: 2, // S
-    65: 3  // A
   };
 
   // Respond to direction keys
@@ -60,11 +52,6 @@ KeyboardInputManager.prototype.listen = function () {
         event.preventDefault();
         self.emit("move", mapped);
       }
-    }
-
-    // R key restarts the game
-    if (!modifiers && event.which === 82) {
-      self.restart.call(self, event);
     }
   });
 
